453a44a92924ed2963c64c7e227cab8bc3e53258
Subhajit basu <basus942@gmail.com>
2023-06-10 15:51:44 +0530

    vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import Layout from "@/components/Layout/Layout";
 import Context from "../context/Context";
+import { Analytics } from "@vercel/analytics/react";
 
 import "@/styles/globals.css";
 import { ProtectedRoute } from "@/context/ProtectedRoute";
@@ -13,6 +14,7 @@ export default function App({ Component, pageProps }) {
         <Context>
           <Layout>
             <Component {...pageProps} />
+            <Analytics />
           </Layout>
         </Context>
       </ProtectedRoute>
